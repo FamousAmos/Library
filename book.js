ce5067828515409b2fc3b3c6364081d8abce0a06
Amos Kenner <akenner92@gmail.com>
2022-08-04 16:54:44 -0500

    Adds book to library

--- book.js
+++ book.js
@@ -1,6 +1,17 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+}
+
+function addBookToLibrary() {
+  const title = prompt("What is the title of the book?");
+  const author = prompt("Who is the author of this book?");
+  const pages = prompt("What is the number of pages?");
+  const read = prompt("Have you read it?");
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
